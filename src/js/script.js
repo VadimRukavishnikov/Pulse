85d908a31b8108e78c56c4aa9a8df441859d0356
Vadim <ironman64@mail.ru>
2022-09-27 17:42:39 +1000

    added sending emails, the up element, the mask for entering numbers on the site, css3 animation. The site is validated.

--- src/js/script.js
+++ src/js/script.js
@@ -49,4 +49,92 @@ $(document).ready(function(){
 		$('.overlay, #order').fadeIn('slow');
 	})
   });
+
+  function validForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: jQuery.validator.format("Введите {0} символа!")
+        },
+        phone: "Пожалуйста, введите свой номер телефона",
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введен адрес почты"
+        }
+      }
+    });
+  };
+
+  validForms('#consultation-form');
+  validForms('#consultation form');
+  validForms('#order form');
+
+  $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+  // $('form').submit(function(e) {
+  //   e.preventDefault();
+  //   $.ajax({
+  //       type: "POST",
+  //       url: "mailer/smart.php",
+  //       data: $(this).serialize()
+  //   }).done(function() {
+  //       $(this).find("input").val("");
+  //       $('#consultation, #order').fadeOut();
+  //       $('.overlay, #thanks').fadeIn('slow');
+
+  //       $('form').trigger('reset');
+  //   });
+  //   return false;
+  // });
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+
+    if ($(this).valid()) {   
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('fast');
+
+            $('form').trigger('reset');
+        });
+    }
+    return false;
+  });
+
+  // Smooth scroll and page up
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1400) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href='#up']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
+
+new WOW().init();
+
 });      
